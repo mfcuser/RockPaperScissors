126a0e7bf11d32dd59c163351740431cf43a523f
mfcuser <modrzewskisebastian1988@gmail.com>
2023-09-09 16:53:35 +0200

    add variables and intro console log

---
+++ script.js
@@ -0,0 +1,23 @@
+let rock; // czy dopisac wartosc?
+let paper; // czy dopisac wartosc?
+let scissors; // czy dopisac wartosc?
+
+let player = ''; // czy dopisac wartosc?
+let computer = ''; // czy dopisac wartosc?
+
+let playerScore = 0;
+let computerScore = 0;
+
+let gameRound = 0;
+
+console.log('Choose rock, paper or scissors');
+
+
+
+function getComputerChoice (params) {
+    // wybiera losowo jedno z trzech - kamien, papier lub nozyce
+}
+
+function randomGenerator() {
+    return Math.floor(Math.random() * 3) + 1; 
+}
