050243fca0da4897795cdf66a77e6bbc44e89552
mfcuser <modrzewskisebastian1988@gmail.com>
2023-09-09 21:56:48 +0200

    add user input prompt

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// ZMIENNE
+
 let rock; // czy dopisac wartosc?
 let paper; // czy dopisac wartosc?
 let scissors; // czy dopisac wartosc?
@@ -10,9 +12,32 @@ let computerScore = 0;
 
 let gameRound = 0;
 
-console.log('Choose rock, paper or scissors');
+// LOGIKA
 
+let userInput = prompt("Choose rock, paper or scissors"); 
+    lowLetters = userInput.toLowerCase();
 
+    if (lowLetters == "rock") {
+        console.log("You choose ROCK");
+    } else if (lowLetters == "paper") { 
+        console.log("You choose PAPER");    
+    } else if  (lowLetters == "scissors") { 
+        console.log("You choose SCISSORS");
+    } else  {
+        console.log("Wrong answer");
+    };
+
+ // ALGORYTM   
+
+userInput;
+// teraz dopisz reakcje komputera
+// wybranie kto wygrywa
+// wynik
+// kolejna runda (petla)
+
+// FUNKCJE
+
+// function itemChoice()
 
 function getComputerChoice (params) {
     // wybiera losowo jedno z trzech - kamien, papier lub nozyce
@@ -20,4 +45,4 @@ function getComputerChoice (params) {
 
 function randomGenerator() {
     return Math.floor(Math.random() * 3) + 1; 
-}
+};
