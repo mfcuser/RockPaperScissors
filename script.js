8b24237fbffd3591ce042c444aadfc7129d21346
mfcuser <modrzewskisebastian1988@gmail.com>
2023-09-10 21:03:31 +0200

    fix algorythm

--- script.js
+++ script.js
@@ -1,48 +1,24 @@
 // ZMIENNE
 
-let playerChoice = getPlayerChoice();
-let computerChoice = getComputerChoice();
+
 
 let playerScore = 0;
 let computerScore = 0;
 
 let gameRound = 0;
 
-
-
-// LOGIKA
-
-/* let userInput = prompt("Choose rock, paper or scissors"); 
-    lowLetters = userInput.toLowerCase();
-
-    if (lowLetters == "rock") {
-        console.log("You choose ROCK");
-    } else if (lowLetters == "paper") { 
-        console.log("You choose PAPER");    
-    } else if  (lowLetters == "scissors") { 
-        console.log("You choose SCISSORS");
-    } else  {
-        console.log("Wrong answer");
-    }; 
-    */
-    
-
  // ALGORYTM   
 
- playerChoice;
- computerChoice;
- roundCalculation();
- getScore();
+ for (let i = 0; i < 5; i++) {
+    let playerChoice = getPlayerChoice();
+    let computerChoice = getComputerChoice();
+    roundCalculation(playerChoice, computerChoice);
+    getScore();
+ }
  
-
-// teraz dopisz reakcje komputera
-// wybranie kto wygrywa
-// wynik
-// kolejna runda (petla)
-
+ console.log("The end!");
+ 
 // FUNKCJE
-
-// function itemChoice()
 
 function getPlayerChoice () {
     let userInput = prompt("Choose rock, paper or scissors");
